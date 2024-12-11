23c337682c1b56f6fa5317402e28373bb7923949
sabxrAL <160590854+sabxrAL@users.noreply.github.com>
2024-12-11 18:50:30 +0100

    res beginning

--- Assets/MenuOpciones.cs
+++ Assets/MenuOpciones.cs
@@ -2,12 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuOpciones : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
 
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+/*
+    private void Start()
+    {
+        int CurrentResolutionIndex = 0;
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string Option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(Option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                CurrentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = CurrentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int ResolutionIndex)
+    {
+        Resolution resolution = resolutions[ResolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+*/
     public void SetMusicVolume(float musicVol)
     {
         audioMixer.SetFloat("music volume", musicVol);
