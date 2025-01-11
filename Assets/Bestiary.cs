c3858cca5374145f1a36f8a7cace86b52b4c77ee
sabxrAL <160590854+sabxrAL@users.noreply.github.com>
2025-01-11 12:16:35 +0100

    bestiary

---
+++ Assets/Bestiary.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bestiary : MonoBehaviour
+{
+    public GameObject[] entries;
+
+    public void EnableEntry(int id)
+    {
+        for (int i = 0; i < entries.Length; i++)
+        {
+            entries[id].SetActive(false);
+
+        }
+        entries[id].SetActive(true);
+    }
+}
